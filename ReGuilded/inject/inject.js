d32eed1f43696b923c7d66c5ee8cc6572ef1e2ac
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2021-06-02 22:57:37 +0300

    Add better logging Clean up some stuff

--- ReGuilded/inject/inject.js
+++ ReGuilded/inject/inject.js
@@ -5,11 +5,11 @@ exports.inject = async(appDir) => {
     if (!existsSync(appDir)) {
         try {
             mkdirSync(appDir);
-
-            writeFileSync(join(appDir, "index.js"), `require(\`${__dirname.replace(RegExp(sep.repeat(2), 'g'), '/')}/../src/reguildedPatcher.js\`)`);
+            // Creates a path for patcher for require statement
+            const patcherPath = join(__dirname.replace(RegExp(sep.repeat(2), 'g'), '/'), '../src/reguildedPatcher.js')
+            // Creates require statement in `index.js`
+            writeFileSync(join(appDir, "index.js"), `require("${patcherPath}");`);
             writeFileSync(join(appDir, "package.json"), JSON.stringify({ name: "guilded", main: "index.js", version: "0.0.0" }));
-
-            return;
         } catch (err) {
             throw new Error(err);
         }
@@ -23,8 +23,6 @@ exports.uninject = async(appDir) => {
     if (existsSync(appDir)) {
         try {
             rmSync(appDir, { recursive: true, force: true});
-
-            return;
         } catch (err) {
             throw new Error(err);
         }
