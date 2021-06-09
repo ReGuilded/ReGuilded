09b37b70a3674ea5f7b982050f4a6d94ffce5fb9
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2021-06-09 22:52:45 +0300

    Move to ~/.reguilded by default

--- guildedModifier/inject/index.js
+++ guildedModifier/inject/index.js
@@ -1,7 +1,13 @@
+const { exec } = require("child_process")
+const argv = require("minimist")(process.argv.slice(2))
+const log4js = require("log4js")
+const path = require("path")
+
+// Additional code
 const { getPlatformModule } = require("./injectUtil");
 const main = require("./main.js");
-const log4js = require("log4js");
 
+// Logger stuff
 global.logger = log4js.getLogger("ReGuilded");
 global.logger.level = "debug";
 const { logger } = global
@@ -12,20 +18,37 @@ let platformModule = getPlatformModule();
  * Performs a given task
  */
 async function mainAsync() {
-    const taskArg = process.argv[2];
-    logger.info("Performing task", taskArg);
-
-    if (["inject", "uninject"].includes(taskArg)) {        
+    // Gets command-line arguments
+    const reguilded = argv.r ?? argv.reguilded
+    // Gets task to do
+    const [taskArg] = argv._;
+    // If task argument is null, return error
+    if(taskArg === undefined) throw new Error("First argument expected")
+    logger.info("Performing task", taskArg)
+    // Checks types of those arguments
+    if(reguilded !== undefined && typeof reguilded !== 'string')
+        throw new TypeError('Argument -r or --reguilded must be a string')
+    // If there is given task, then run it
+    if (main[taskArg] !== null) {        
         logger.info("Force closing Guilded");
-
+        // Creates path for ReGuilded
+        const reguildedPath = path.resolve(
+            reguilded ??
+            // if variable `reguilded` is empty, get default path instead
+            path.join(process.env.APPDATA ?? process.env.HOME, ".reguilded")
+        )
         try {
-            await main[taskArg](platformModule)
+            // Kills Guilded's process
+            exec(platformModule.closeGuilded);
+            // Executes the task
+            await main[taskArg](platformModule.getAppDir(), reguildedPath)
             // Tells us that it succeeded
             logger.info("Task", taskArg, "has been successful");
         } catch(err) {
             logger.error("Failed to do task", taskArg);
             logger.fatal(err);
         }
+    // Otherwise shout that no task was found
     } else logger.error("Unknown task", taskArg);
 }
 
--- guildedModifier/inject/main.js
+++ guildedModifier/inject/main.js
@@ -1,64 +1,69 @@
 const { writeFileSync, existsSync, mkdirSync, rmSync } = require("fs");
+const { copySync } = require("fs-extra");
+const { spawnSync } = require("child_process");
 const { join, sep } = require("path");
-const { exec } = require("child_process");
-const { spawnSync } = require('child_process');
 
-function rootPerms(path) {
+function rootPerms(path, reguildedDir) {
     // Warns us about this
     global.logger.warn(`ReGuilded Linux requires root permissions to create, modify or delete '${path}'`)
     // Goes into sudo mode
-    // FIXME Won't work for doas
-    spawnSync('sudo', process.argv, { stdio: 'inherit' })
+    // FIXME Won't work for non-sudo users
+    spawnSync('sudo', process.argv.slice(0, 3).concat(['-r', reguildedDir]), { stdio: 'inherit' })
     process.exit(0)
 }
-exports.inject = async(platformModule) => {
-    const appDir = platformModule.getAppDir();
-
-    if (!existsSync(appDir)) {
+/**
+ * Injects ReGuilded into Guilded.
+ * @param {String} guildedDir Path to Guilded's resource/app directory
+ * @param {String} reguildedDir Path to ReGuilded's configuration directory
+ */
+exports.inject = async(guildedDir, reguildedDir) => {
+    // If there is no injection present, inject
+    if (!existsSync(guildedDir)) {
         try {
+            // Creates path for 'src' directory
+            const src = join(__dirname, '../src')
+            // Copies src stuff to ~/.reguilded
+            copySync(src, reguildedDir, { recursive: true, errorOnExist: false, overwrite: true })
+
             // If this is on Linux, do it in sudo perms
             if (process.platform === 'linux' && process.getuid() !== 0)
-                rootPerms(appDir);
+                rootPerms(guildedDir, reguildedDir);
             // Creates the "app" directory in Guilded's "resources" directory.
-            mkdirSync(appDir);
+            mkdirSync(guildedDir);
 
             // Creates a path for patcher for require statement
-            const patcherPath = join(__dirname, "../src/reguildedPatcher.js").replace(RegExp(sep.repeat(2), "g"), "/");
+            const patcherPath = join(reguildedDir, "reguildedPatcher.js").replace(RegExp(sep.repeat(2), "g"), "/");
 
             // Creates require statement in `index.js`
-            writeFileSync(join(appDir, "index.js"), `require("${patcherPath}");`);
-            writeFileSync(join(appDir, "package.json"), JSON.stringify({ name: "guilded", main: "index.js", version: "0.0.0" }));
-
-            // Kills Guilded's process
-            exec(platformModule.closeGuilded);
+            writeFileSync(join(guildedDir, "index.js"), `require("${patcherPath}");`);
+            writeFileSync(join(guildedDir, "package.json"), JSON.stringify({ name: "guilded", main: "index.js", version: "0.0.0" }));
         } catch (err) {
             // If there was an error, try uninjecting ReGuilded
-            //await exports.uninject(platformModule);
+            await exports.uninject(guildedDir, reguildedDir);
 
             throw new Error(err);
         }
-    } else {
-        throw new Error("There is already an injection.");
-    }
+    // Otherwise, throw an error
+    } else throw new Error("There is already an injection.");
 }
 
-exports.uninject = async(platformModule) => {
-    const appDir = platformModule.getAppDir();
-
-    if (existsSync(appDir)) {
+/**
+ * Removes any injections present in Guilded.
+ * @param {String} guildedDir Path to Guilded's resource/app directory
+ * @param {String} _ Path to ReGuilded's configuration directory
+ */
+exports.uninject = async(guildedDir, _) => {
+    // If there is an injection, then remove the injection
+    if (existsSync(guildedDir)) {
         try {
             // If this is on Linux, do it in sudo perms
             if (process.platform === 'linux' && process.getuid() !== 0)
-                rootPerms(appDir);
-            // Deletes "app" directory in Guilded's "resources" directory.
-            rmSync(appDir, { recursive: true, force: true});
-
-            // Kills Guilded's process
-            exec(platformModule.closeGuilded);
+                rootPerms(guildedDir, reguildedDir);
+            // Removes JS file
+            rmSync(guildedDir, { force: true, recursive: true });
         } catch (err) {
             throw new Error(err);
         }
-    } else {
-        throw new Error("There is no injection.");
-    }
+    // Otherwise, throw
+    } else throw new Error("There is no injection.");
 }
