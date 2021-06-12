dc518aa5d94e3ebf4fd9f1ac8d96a2de95c8e2ed
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2021-06-12 23:44:53 +0300

    Fix appVersion missing on Linux version

--- guildedModifier/src/preload/preload.js
+++ guildedModifier/src/preload/preload.js
@@ -1,6 +1,6 @@
 const { ipcRenderer } = require("electron");
 
-const ReGuilded = require("../ReGuilded");
+const ReGuilded = require("../ReGuilded/Index.js");
 global.ReGuilded = new ReGuilded();
 
 const preload = ipcRenderer.sendSync('REGUILDED_GET_PRELOAD');
