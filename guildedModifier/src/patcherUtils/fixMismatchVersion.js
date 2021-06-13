8b360a5a36b6ebc158370e2173b7e99e54f35286
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2021-06-13 16:32:25 +0300

    Automatic mismatch fix on Linux

--- guildedModifier/src/patcherUtils/fixMismatchVersion.js
+++ guildedModifier/src/patcherUtils/fixMismatchVersion.js
@@ -1,18 +1,32 @@
 const { readFileSync, writeFileSync } = require("fs")
+const { spawnSync } = require("child_process")
+const { join } = require("path")
 
 module.exports = (guildedPackage, reguildedPackage) => {
     const reguildedPackageSource = readFileSync(reguildedPackage);
     var reguildedPackageJSON = JSON.parse(reguildedPackageSource);
 
     if (reguildedPackageJSON.version !== guildedPackage.version) {
-        // If this is on Linux, throw an error
-        if (process.platform === 'linux' && process.getuid() !== 0)
-            throw new Error(`Version mismatch between ReGuilded and Guilded. ReGuilded: ${reguildedPackageJSON.version} vs Guilded: ${guildedPackage.version}. Linux version of ReGuilded requires manual editing.`)
+        //throw new Error(`Version mismatch between ReGuilded and Guilded. ReGuilded: ${reguildedPackageJSON.version} vs Guilded: ${guildedPackage.version}. Linux version of ReGuilded requires manual editing.`)
         reguildedPackageJSON.version = guildedPackage.version;
 
-        writeFileSync(reguildedPackage, JSON.stringify(reguildedPackageJSON, null, 2));
+        // Turns made package file into JSON
+        const packageJson = JSON.stringify(reguildedPackageJSON);
+        // If this is on Linux, create file and move it to Guilded
+        if (process.platform === 'linux' && process.getuid() !== 0) {
+            // Creates file's path
+            const filePath = join(__dirname, '_package.json')
+            // Creates a temporary file
+            writeFileSync(filePath, packageJson)
+            // Replaces ReGuilded's package JSON with the temporary file
+            spawnSync('sudo', ['mv', filePath, reguildedPackage], { stdio: 'inherit' })
+        }
+        // Else, don't ask for sudo permissions
+        else
+            writeFileSync(reguildedPackage, packageJson);
 
         // TODO: Find a way to automatically rerun the process correctly after changing the version.
-        throw new Error("There was a mismatch version between ReGuilded and Guilded. If you're seeing this, we've successfully fixed this, and all you need to do is relaunch Guilded.");
+        console.warn("There was a mismatch version between ReGuilded and Guilded. If you're seeing this, we've successfully fixed this, and all you need to do is relaunch Guilded.");
+        process.exit(0)
     }
 }
