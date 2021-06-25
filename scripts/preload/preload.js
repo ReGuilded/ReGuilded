2b0ec35fac3b6e3ceed402df8d4d5e672f6b8511
Two <njsolomonoh@gmail.com>
2021-06-25 19:14:04 -0400

    Add files via upload

--- scripts/preload/preload.js
+++ scripts/preload/preload.js
@@ -15,14 +15,16 @@ if (document.readyState === "loading") {
 }
 
 // Sets ReGuilded staff members
-fetch('https://gist.githubusercontent.com/IdkGoodName/feb175e9d74320cb61a72bf2ad60fc81/raw/b9fd6edd73da1634530872b407ed7ec123453ce2/staff.json')
+fetch(`https://gist.githubusercontent.com/IdkGoodName/feb175e9d74320cb61a72bf2ad60fc81/raw/b9fd6edd73da1634530872b407ed7ec123453ce2/staff.json?v=${Date.now()}&t=${Math.random().toString(36).substring(2, 15)}`)
     .then(x => x.json())
     .then(x => badges.members.staff = x)
 
 document.addEventListener('readystatechange', () => {
     // When bundle loads
     if(document.readyState === 'interactive') {
-        global.bundle.addEventListener('load', () => {
+        global.bundle.addEventListener('load', async () => {
+            badges.members.theme_developers = global.ReGuilded.themesManager.getThemeDevelopers()
+
             // FIXME: NO SVGS ANYMORE BECAUSE OF THIS
             // Push a new module to it
             global.webpackJsonp.push([ [151], { 1393:
