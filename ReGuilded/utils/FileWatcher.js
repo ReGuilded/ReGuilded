30550696f921bfa95880986c3ee8cc9661d00020
MasicoreLord <personaldesignertechie@gmail.com>
2021-08-03 22:46:33 -0400

    Rolled back FileWatcher to what's live

--- ReGuilded/utils/FileWatcher.js
+++ ReGuilded/utils/FileWatcher.js
@@ -4,8 +4,7 @@ module.exports = class FileWatcher {
     constructor(file, reload, id) {
         watch(file, (event, filename) => {
             if (filename && event === "change") {
-                setTimeout(() => reload(id), 100)
-                //reload(id);
+                reload(id);
             }
         });
     }
