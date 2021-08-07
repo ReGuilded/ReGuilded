552f36b9a880d4a8cb126a5ad0cf7b7678a34a61
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2021-08-07 15:40:37 -0400

    Revert "Fix removed addons and themes being loaded"
    This reverts commit 1730f63f24ae44aa577577ed8a2b383bf163c6aa.

--- ReGuilded/managers/extension.js
+++ ReGuilded/managers/extension.js
@@ -61,21 +61,17 @@ module.exports = class ExtensionManager extends EventEmitter {
     loadAll() {
         // Loads all found enabled extensions
         for (let ext of this.enabled) {
-            const extension = this.all.find(x => x.id === ext);
-            // Make sure it exists
-            if(extension) this.load(extension);
+            const extension = this.all.find(x => x.id == ext);
+            this.load(extension);
         }
     }
     /**
      * Removes ReGuilded themes from Guilded.
      */
     unloadAll() {
-        // Get all existing extensions
-        const existing = this.all.map(x => x.id);
-        // Unload all existing extensions
-        for (let id of this.enabled)
-            if(existing.contains(id))
-                this.unload(id);
+        // Gets all enabled extensions
+        // Unloads an extension
+        for (let id of this.enabled) this.unload(id);
     }
     /**
      * Gets path of an extension.
