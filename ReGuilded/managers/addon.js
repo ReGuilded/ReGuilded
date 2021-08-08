1215e25ca7ce6e20e6e00927810765e6e4e06986
Nathaniel M <nmoratto629@gmail.com>
2021-08-07 17:19:39 -0700

    Changing words. "addon" to "add-on"

--- ReGuilded/managers/addon.js
+++ ReGuilded/managers/addon.js
@@ -4,12 +4,12 @@ const { join } = require("path");
 const { existsSync } = require("fs");
 
 /**
- * Manager that manages ReGuilded's addons
+ * Manager that manages ReGuilded's add-ons
  */
 module.exports = class AddonManager extends ExtensionManager {
     /**
-     * Manager that manages ReGuilded's addons
-     * @param {String} addonsDir Path to the directory that holds addons
+     * Manager that manages ReGuilded's add-ons
+     * @param {String} addonsDir Path to the directory that holds add-ons
      * @param {ReGuilded} reGuilded Instance of ReGuilded that created this manager
      */
     constructor(addonsDir, reGuilded) {
@@ -18,24 +18,24 @@ module.exports = class AddonManager extends ExtensionManager {
     }
 
     /**
-     * Initiates addons for ReGuilded and addon manager
-     * @param {String[]} enabled An array of enabled addons.
+     * Initiates addons for ReGuilded and add-on manager
+     * @param {String[]} enabled An array of enabled add-ons.
      */
     init(enabled = []) {
-        console.log("Initiating addon manager");
-        // Gets a list of addon directories
+        console.log("Initiating add-on manager");
+        // Gets a list of add-on directories
         const addons = super.getDirs(enabled);
-        // Gets every theme directory
+        // Gets every add-on directory
         for (let i in addons) {
             const addon = addons[i]
             // Try-catch errors to prevent conflicts with other plugins
             try {
                 console.log(`Found addon directory '${addon.name}'`);
-                // Gets the path of the addon
+                // Gets the path of the add-on
                 const addonPath = super.getPath(addon.name);
                 // Gets path of the main JS file
                 const jsPath = join(addonPath, "main.js");
-                // If it doesn't have main file, it's not an addon and ignore it
+                // If it doesn't have main file, it's not an add-on and ignore it
                 if(!existsSync(jsPath)) continue
                 // Require the main file
                 let main = require(jsPath);
@@ -44,31 +44,31 @@ module.exports = class AddonManager extends ExtensionManager {
                 
                 // Check if the preinit function exists to prevent errors
                 if (typeof(main.preinit) === "function") {
-                    // Pre-initialize addon
+                    // Pre-initialize add-on
                     main.preinit(this.parent, this);
                     // Sets directory's name
                     main.dirname = addonPath;
                     // Setup watcher
                     main.watcher = new FileWatcher(addonPath, this.reload.bind(this), main.id);
-                    // Push it to the list of addons
+                    // Push it to the list of add-ons
                     this.all.push(main);
-                    // Emit addon load event
+                    // Emit add-on load event
                     this.emit("load", main);
                 }
-                else console.error("Addon has no preinit function or has invalid formatting:", addon.id, '-', addon.name);
+                else console.error("Add-on has no preinit function or has invalid formatting:", addon.id, '-', addon.name);
             }
             catch (e) {
-                console.error("Failed to initialize addon by ID", addon.id, e);
+                console.error("Failed to initialize add-on by ID", addon.id, e);
             }
             // Checks if it's the last item
             this.checkLoaded(i, addons.length);
         }
-        // Loads all addons
+        // Loads all add-ons
         this.loadAll();
     }
     /**
-     * Loads a ReGuilded addon.
-     * @param {{id: String, name: String, init: Function, uninit: Function}} addon Addon to load onto Guilded
+     * Loads a ReGuilded add-on.
+     * @param {{id: String, name: String, init: Function, uninit: Function}} addon add-on to load onto Guilded
      */
     load(addon) {
         // Try-catch errors to prevent conflicts with other plugins
@@ -77,16 +77,16 @@ module.exports = class AddonManager extends ExtensionManager {
             addon.init(this.parent, this, this.parent.webpackManager);
         }
         catch (e) {
-            console.error("Failed to load addon by ID", addon.id, e);
+            console.error("Failed to load add-on by ID", addon.id, e);
         }
     }
     /**
-     * Unloads/removes a ReGuilded addon.
-     * @param {{id: String, name: String, init: Function, uninit: Function}} addon Addon to load onto Guilded
+     * Unloads/removes a ReGuilded add-on.
+     * @param {{id: String, name: String, init: Function, uninit: Function}} addon add-on to load onto Guilded
      */
     unload(addon) {
         try {
-            console.log("Unloading addon by ID", addon.id);
+            console.log("Unloading add-on by ID", addon.id);
             addon.uninit();
             
             const cachedModules = Object.keys(require.cache)
@@ -94,19 +94,19 @@ module.exports = class AddonManager extends ExtensionManager {
             cachedModules.forEach(moduleId => delete require.cache[moduleId]);
         }
         catch (e) {
-            console.error("Failed to unload an addon by ID", addon.id, e);
+            console.error("Failed to unload an add-on by ID", addon.id, e);
         }
     }
     /**
-     * Reloads a ReGuilded addon.
-     * @param {{id: String}} id id of addon to reload on Guilded
+     * Reloads a ReGuilded add-on.
+     * @param {{id: String}} id id of add-on to reload on Guilded
      */
     reload(id) {
         let addon = this.all.find(addon => addon.id === id);
-        if (!addon) addon = { name: `Invalid Addon (${id})`, invalid: true };
+        if (!addon) addon = { name: `Invalid Add-on (${id})`, invalid: true };
         try {
             if(addon.invalid) throw new Error("Not installed or loaded");
-            console.log("Reloading addon by ID", id);
+            console.log("Reloading add-on by ID", id);
             // Unitilize addon
             addon.uninit();
             
@@ -119,14 +119,14 @@ module.exports = class AddonManager extends ExtensionManager {
             if (typeof(reloadedAddon.preinit) !== "function" && reloadedAddon.default) reloadedAddon = reloadedAddon.default;
             // Check if the preinit function still exists to prevent errors
                 if (typeof(reloadedAddon.preinit) === "function") {
-                    // Pre-initialize addon
+                    // Pre-initialize add-on
                     reloadedAddon.preinit(this.parent, this);
-                    // Initialize addon
+                    // Initialize add-on
                     reloadedAddon.init(this.parent, this, this.parent.webpackManager);
                 }
-                else console.error("Addon has no preinit function or is formatted invalidly!", addon.name);
+                else console.error("Add-on has no preinit function or is formatted invalidly!", addon.name);
         } catch(e) {
-            console.error("Addon failed to reload!", addon.name, e);
+            console.error("Add-on failed to reload!", addon.name, e);
         }
     }
 };
