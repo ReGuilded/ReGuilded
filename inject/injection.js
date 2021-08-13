8375c664884c762eda3bb60d17d4496bb783705b
Nathaniel M <nmoratto629@gmail.com>
2021-08-13 16:32:35 -0700

    Changed index & package files to be only made if app is successfully.

--- inject/injection.js
+++ inject/injection.js
@@ -1,14 +1,23 @@
-import { writeFileSync, mkdirSync } from "fs";
+import { writeFileSync, mkdir } from "fs";
 import { join, sep } from "path";
+import log4js from "log4js";
+
+// Logger stuff
+const logger = log4js.getLogger("ReGuilded");
 
 export default function(guildedDir, reguildedDir) {
     // Creates the "app" directory in Guilded's "resources" directory.
-    mkdirSync(guildedDir);
+    mkdir(guildedDir, (err) => {
+        if (!err) {
+            // Creates a path for patcher for require statement
+            const patcherPath = join(reguildedDir, "scripts/reguildedPatcher.js").replace(RegExp(sep.repeat(2), "g"), "/");
 
-    // Creates a path for patcher for require statement
-    const patcherPath = join(reguildedDir, "scripts/reguildedPatcher.js").replace(RegExp(sep.repeat(2), "g"), "/");
-
-    // Creates require statement in `index.js`
-    writeFileSync(join(guildedDir, "index.js"), `require("${patcherPath}");`);
-    writeFileSync(join(guildedDir, "package.json"), JSON.stringify({ name: "Guilded", main: "index.js" }));
+            // Creates require statement in `index.js`
+            writeFileSync(join(guildedDir, "index.js"), `require("${patcherPath}");`);
+            writeFileSync(join(guildedDir, "package.json"), JSON.stringify({ name: "Guilded", main: "index.js" }));
+        } else {
+            logger.error("There was an error creating the app directory.")
+            logger.fatal(err);
+        }
+    })
 }
