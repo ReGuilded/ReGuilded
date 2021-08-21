075bec51513efa5567f3420a75d8b16e737fd3f6
Bloom Sirenix <66263341+bloomsirenix@users.noreply.github.com>
2021-08-21 18:34:24 +0200

    added contributor and project lead badges

--- ReGuilded/badges.js
+++ ReGuilded/badges.js
@@ -21,6 +21,14 @@ module.exports.genBadgeGetter = (defaultBadges) =>
         if (module.exports.members.dev.includes(this.userInfo.id) && !badges?.includes?.(module.exports.dev))
             // Pushes the new badge
             (badges || (badges = [])).push(module.exports.dev);
+        // Checks if the user is ReGuilded Project Lead
+        if (module.exports.members.lead.includes(this.userInfo.id) && !badges?.includes?.(module.exports.lead) &&  !module.exports.members.dev.includes(this.userInfo.id))
+            // Pushes the new badge
+            (badges || (badges = [])).push(module.exports.lead);
+        // Checks if the user is ReGuilded Project Contrubtor
+        if (module.exports.members.contrib.includes(this.userInfo.id) && !badges?.includes?.(module.exports.contrib) && !module.exports.members.dev.includes(this.userInfo.id) && !module.exports.members.lead.includes(this.userInfo.id) )
+            // Pushes the new badge
+            (badges || (badges = [])).push(module.exports.contrib);
         // Return the badge array
         return badges;
     };
@@ -38,9 +46,31 @@ module.exports.dev = {
     text: "ReDev",
     style: { backgroundColor: "#10171F", color: "#CC5555" },
 };
+module.exports.lead = {
+    icon: "https://raw.githubusercontent.com/ReGuilded/ReGuilded/main/logo/ReGuilded.png",
+    // Sets the name of the badge for getting this badge
+    name: "ReGuildedProjLead",
+    // What is displayed when you hover over the badge
+    tooltipText: "ReGuilded Project Lead",
+    // Adds the display text/name
+    text: "ReLead",
+    style: { backgroundColor: "#10171F", color: "#CC5555" },
+};
+module.exports.contrib = {
+    icon: "https://raw.githubusercontent.com/ReGuilded/ReGuilded/main/logo/ReGuilded.png",
+    // Sets the name of the badge for getting this badge
+    name: "ReGuildedProjContrib",
+    // What is displayed when you hover over the badge
+    tooltipText: "ReGuilded Contributor",
+    // Adds the display text/name
+    text: "ReContrib",
+    style: { backgroundColor: "#10171F", color: "#CC5555" },
+};
 /**
  * People who hold defined badges
  */
 module.exports.members = {
     dev: [],
+    lead: [],
+    contrib: ["AnbaMOyA"],
 };
