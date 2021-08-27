76a14d7256c27e399d6f7b678f591c6d31bb9b9c
Nathaniel M <nmoratto629@gmail.com>
2021-08-27 16:48:17 -0700

    Revert "Set libs folder as a node modules folder."
    This reverts commit df916b01fdbc2b05a8a43516caa6fe51e47a302c.

--- scripts/preload/preload.js
+++ scripts/preload/preload.js
@@ -2,11 +2,7 @@ const copyDir = require("../../ReGuilded/libs/copy-dir");
 const badges = require("../../ReGuilded/badges.js");
 const { ipcRenderer } = require("electron");
 const { access } = require("fs");
-const { dirname, join } = require("path");
-
-// Set ReGuilded/libs as another node_modules folder.
-const libs = dirname(join("../../ReGuilded", "libs"));
-process.mainModule.paths.push(libs);
+const { join } = require("path");
 
 // Get ReGuilded's class.
 const ReGuilded = require("../../ReGuilded");
