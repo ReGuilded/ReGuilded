71c5d1692540450b98002b270759f1c50711c2b1
MasicoreLord <11468845+MasicoreLord@users.noreply.github.com>
2022-01-03 15:59:43 -0500

    Revert "refactor to more closely match code style and prep for webpack patch in preload"
    This reverts commit 950db9732edd7be03f6ee133c010bde64e0f8e1e.

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,5 @@
 import { members } from "./core/badges-flairs";
-//import webpackPush from "./webpackInject";
+import webpackPush from "./webpackInject";
 import ReGuilded from "./core/ReGuilded";
 //import { ipcRenderer } from "electron";
 
@@ -10,24 +10,24 @@ window.ReGuilded = new ReGuilded();
 //     require(preload);
 // }
 
-// function setPush(obj) {
-//     Object.defineProperty(window.webpackJsonp, "push", obj)
-// }
+function setPush(obj) {
+    Object.defineProperty(window.webpackJsonp, "push", obj)
+}
 
-// document.addEventListener("readystatechange", () => {
-//     // To wait for the bundle to be created
-//     if (document.readyState === "interactive")
-//         // Wait when bundle loads
-//         window.bundle.addEventListener("load", () => {
-//             // Saves the old push
-//             window.webpackJsonp._push = window.webpackJsonp.push;
+document.addEventListener("readystatechange", () => {
+    // To wait for the bundle to be created
+    if (document.readyState === "interactive")
+        // Wait when bundle loads
+        window.bundle.addEventListener("load", () => {
+            // Saves the old push
+            window.webpackJsonp._push = window.webpackJsonp.push;
 
-//             setPush({
-//                 get: () => webpackPush.bind(window.webpackJsonp),
-//                 set: (value) => setPush({get: () => value})
-//             });
-//         });
-// });
+            setPush({
+                get: () => webpackPush.bind(window.webpackJsonp),
+                set: (value) => setPush({get: () => value})
+            });
+        });
+});
 // Fetch ReGuilded things
 (async () => {
     // Global badge holders
