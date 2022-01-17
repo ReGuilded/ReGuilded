e3e55b55ccac4c787563dd546aa9c34acec1b685
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2022-01-17 14:19:18 +0200

    Inject ReGuilded even if it's late

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,33 +2,41 @@ import { members } from "./core/badges-flairs";
 import ReGuilded from "./core/ReGuilded";
 import webpackPush from "./webpackInject";
 
-//import { ipcRenderer } from "electron";
-
-// const preload = ipcRenderer.sendSync("reguilded-preload");
-// if (preload) {
-//     require(preload);
-// }
-
 window.ReGuilded = new ReGuilded();
 
 function setPush(obj) {
     Object.defineProperty(window.webpackJsonp, "push", obj);
 }
 
-document.addEventListener("readystatechange", () => {
+let hasInjected = false;
+function setUpWebpackInjection() {
     // To wait for the bundle to be created
     if (document.readyState === "interactive")
         // Wait when bundle loads
-        window.bundle.addEventListener("load", () => {
-            // Saves the old push
-            window.webpackJsonp._push = window.webpackJsonp.push;
+        window.bundle.addEventListener("load", injectWebpackJsonp);
+    // Still try injecting even if it was too late
+    else if (!hasInjected && document.readyState === "complete") {
+        console.warn(
+            "WebpackJsonp injection is too late. Still injecting. This may require loading a bundle that has not been loaded yet. If ReGuilded hasn't loaded yet, make sure to load settings or area that you have not yet viewed or refresh Guilded."
+        );
+        injectWebpackJsonp();
+    }
+}
+function injectWebpackJsonp() {
+    hasInjected = true;
 
-            setPush({
-                get: () => webpackPush.bind(window.webpackJsonp),
-                set: value => setPush({ get: () => value })
-            });
-        });
-});
+    // Saves the old push
+    window.webpackJsonp._push = window.webpackJsonp.push;
+
+    setPush({
+        get: () => webpackPush.bind(window.webpackJsonp),
+        set: value => setPush({ get: () => value })
+    });
+}
+
+// Inject if it's possible yet, or wait for it to be possible
+if (document.readyState !== "loading") setUpWebpackInjection();
+else document.addEventListener("readystatechange", setUpWebpackInjection);
 
 // Fetch ReGuilded things
 (async () => {
