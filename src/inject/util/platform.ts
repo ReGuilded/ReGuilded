1abbf4acc45935e8de80b9680dd07a95a0a76235
Nathaniel M <nmoratto629@gmail.com>
2022-02-04 22:49:45 -0800

    Reordered code.

--- src/inject/util/platform.ts
+++ src/inject/util/platform.ts
@@ -2,10 +2,10 @@ import { join } from "path";
 
 const platforms = {
     linux: {
-        resourcesDir: "/opt/Guilded/resources",
         close: "killall guilded",
-        reguildedDir: "/usr/local/share/ReGuilded",
         appName: "guilded",
+        reguildedDir: "/usr/local/share/ReGuilded",
+        resourcesDir: "/opt/Guilded/resources",
         get appDir() {
             return join(this.resourcesDir, "app")
         },
@@ -14,10 +14,10 @@ const platforms = {
         }
     },
     darwin: {
-        resourcesDir: "/Applications/Guilded.app/Contents/Resources",
         close: "killall Guilded",
-        reguildedDir: "/Applications/ReGuilded",
         appName: "Guilded",
+        reguildedDir: "/Applications/ReGuilded",
+        resourcesDir: "/Applications/Guilded.app/Contents/Resources",
         get appDir() {
             return join(this.resourcesDir, "app");
         },
@@ -26,13 +26,13 @@ const platforms = {
         }
     },
     win32: {
-        get resourcesDir() {
-            return join(process.env.LOCALAPPDATA, "Programs/Guilded/resources");
-        },
         close: "taskkill /f /IM Guilded.exe >nul",
         appName: "Guilded.exe",
         get reguildedDir() {
             return join(process.env.ProgramW6432, "ReGuilded");
+        },
+        get resourcesDir() {
+            return join(process.env.LOCALAPPDATA, "Programs/Guilded/resources");
         },
         get appDir() {
             return join(this.resourcesDir, "app");
@@ -42,8 +42,16 @@ const platforms = {
         },
     }
 }
-const current: { resourcesDir: string, close: string, appName: string, reguildedDir: string, appDir: string, open: string } | undefined
-             = platforms[process.platform];
+
+const current: {
+    close: string,
+    appName: string,
+    reguildedDir: string,
+    resourcesDir: string,
+    appDir: string,
+    open: string
+} | undefined = platforms[process.platform];
+
 if (!current)
     // TODO: Possible make it so this also opens a window on the default browser with a prefilled out issue on GitHub.
     throw new Error(`Unsupported platform, ${process.platform}. Please submit a new issue`);
