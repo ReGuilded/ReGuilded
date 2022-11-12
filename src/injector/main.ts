c0bad62377ac17d968207d7181782fcf9a6cda7a
Nathaniel M <me@itznxthaniel.dev>
2022-11-11 21:48:41 -0800

    refactor: cleaned up injection check

--- src/injector/main.ts
+++ src/injector/main.ts
@@ -1,5 +1,6 @@
 // Modules
 import { closeGuildedCommand, getResourcesDir, openGuildedCommand } from "./util/utilFunctions";
+import { access, constants } from "fs/promises";
 import platform from "./util/platform";
 import minimist from "minimist";
 import { join } from "path";
@@ -59,11 +60,18 @@ const utilInfo: {
  * Async function so we can await the util functions. (Since they're kind-of important.)
  */
 (async function () {
+  // Populate utilInfo object.
   utilInfo.openCommand = await openGuildedCommand(utilInfo.guildedAppName, utilInfo.guildedDir);
   utilInfo.closeCommand = await closeGuildedCommand(utilInfo.guildedAppName);
   utilInfo.resourcesDir = await getResourcesDir(utilInfo.guildedDir);
   utilInfo.appDir = join(utilInfo.resourcesDir, "app");
 
-  console.log("TASK:", argv.task);
-  console.log(utilInfo);
+  try {
+    await access(utilInfo.appDir, constants.F_OK);
+
+    if (argv.task === "inject")
+      return console.error(`Called Inject, but ReGuilded is already injected at ${utilInfo.appDir}`);
+  } catch (err) {
+    if (argv.task === "uninject") return console.error(`Called Uninject, but ReGuilded is not injected`);
+  }
 })();
