ReGuilded/ReGuilded

commit 5bfde2d078388dfe3cda5bdd9ddb65d9cec1d695

feat: task execution.
Warning: Contains a hanging process when reopening Guilded.

src/injector/main.ts
@@ -5,6 +5,7 @@ import { exec as sudoExec } from "sudo-prompt";
 import platform from "./util/platform";
 import { exec } from "child_process";
 import { join } from "path";
+import tasks from "./tasks";
 
 // Types
 import type { UtilInfo } from "../typings";
@@ -144,6 +145,7 @@ function isGuildedRunning() {
    */
   try {
     const testDir = join(utilInfo.reguildedDir, "../_rgTestDir");
+
     await mkdir(testDir);
     await rmdir(testDir, { recursive: false });
   } catch (err) {
@@ -158,6 +160,7 @@ function isGuildedRunning() {
    */
   try {
     const testDir = join(utilInfo.resourcesDir, "_rgTestDir");
+
     await mkdir(testDir);
     await rmdir(testDir, { recursive: false });
   } catch (err) {
@@ -193,8 +196,22 @@ function isGuildedRunning() {
         });
       }, 500);
     } else resolve();
-  }).then(() => {
+  }).then(async () => {
     argv.debug && console.log("Continuing Task...");
-    // TODO: Run User's Desired Task.
+
+    try {
+      await tasks[argv.task](utilInfo);
+    } catch (err) {
+      console.error(err);
+    }
+
+    exec(utilInfo.openCommand, (err, stdout, stderr) => {
+      if (err || stderr) console.error(err || stderr);
+
+      // FIX: Process not exiting.
+      process.exit();
+    });
+
+    console.log(`Task ${argv.task} executed successfully!`);
   });
 })();

src/injector/tasks/index.ts
@@ -1,0 +1,5 @@
+import uninject from "./uninject";
+import inject from "./inject";
+import update from "./update";
+
+export default { uninject, inject, update };

src/injector/tasks/inject.ts
@@ -1,0 +1,9 @@
+import type { UtilInfo } from "../../typings";
+
+export default function inject(utilInfo: UtilInfo) {
+  return new Promise<void>((resolve, reject) => {
+    console.log("INJECT:\n", utilInfo);
+
+    resolve();
+  });
+}

src/injector/tasks/uninject.ts
@@ -1,0 +1,9 @@
+import type { UtilInfo } from "../../typings";
+
+export default function uninject(utilInfo: UtilInfo) {
+  return new Promise<void>((resolve, reject) => {
+    console.log("UNINJECT:\n", utilInfo);
+
+    resolve();
+  });
+}

src/injector/tasks/update.ts
@@ -1,0 +1,9 @@
+import type { UtilInfo } from "../../typings";
+
+export default function update(utilInfo: UtilInfo) {
+  return new Promise<void>((resolve, reject) => {
+    console.log("UPDATE:\n", utilInfo);
+
+    resolve();
+  });
+}

commit 052a8658352a716bd9c3631c530702c88e56b6e9

fix: fixed hanging bug introduced in 5bfde2d0
I wanted to avoid having another loop, but it's fine.

src/injector/main.ts
@@ -2,8 +2,8 @@
 import { closeGuildedCommand, getResourcesDir, openGuildedCommand } from "./util/utilFunctions";
 import { access, mkdir, rmdir, constants } from "fs/promises";
 import { exec as sudoExec } from "sudo-prompt";
+import { exec, spawn } from "child_process";
 import platform from "./util/platform";
-import { exec } from "child_process";
 import { join } from "path";
 import tasks from "./tasks";
 
@@ -16,6 +16,9 @@ import type { UtilInfo } from "../typings";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import minimist from "minimist";
+
+// Tasks that need Guilded to be closed when executing.
+const specialTasks = ["inject", "uninject"];
 
 /**
  * Command Line Arguments:
@@ -174,7 +177,7 @@ function isGuildedRunning() {
    * Doing so while Guilded is running would result in an error, relating to the ASAR currently being in used.
    */
   new Promise<void>((resolve) => {
-    if (["inject", "uninject"].includes(argv.task)) {
+    if (specialTasks.includes(argv.task)) {
       // Relays to the user that Guilded needs to be closed, and closes Guilded.
       console.log(`Task ${argv.task} requires Guilded to be closed. Closing Guilded now...`);
       exec(utilInfo.closeCommand);
@@ -205,13 +208,26 @@ function isGuildedRunning() {
       console.error(err);
     }
 
-    exec(utilInfo.openCommand, (err, stdout, stderr) => {
-      if (err || stderr) console.error(err || stderr);
+    console.log(`Task ${argv.task} executed Successfully.`);
 
-      // FIX: Process not exiting.
-      process.exit();
-    });
+    // Only attempt to reopen Guilded if it was closed.
+    if (specialTasks.includes(argv.task)) {
+      exec(utilInfo.openCommand);
 
-    console.log(`Task ${argv.task} executed successfully!`);
+      // Same process as closeCheckInterval but waits until Guilded is detected as opened.
+      const openCheckInterval = setInterval(() => {
+        argv.debug && console.log("Checking if Guilded is Opened...");
+
+        isGuildedRunning().then((isGuildedRunning) => {
+          if (isGuildedRunning) {
+            clearInterval(openCheckInterval);
+
+            argv.debug && console.log("Guilded is Opened.");
+
+            process.exit();
+          }
+        });
+      }, 500);
+    } else process.exit();
   });
 })();