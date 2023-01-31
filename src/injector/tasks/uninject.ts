87dde700530114fda8ddbe295737af75e1dca581
Nathaniel M <me@itznxthaniel.dev>
2023-01-30 23:42:05 -0800

    feat: UNINJECTION NOW WORKS! :tada:

--- src/injector/tasks/uninject.ts
+++ src/injector/tasks/uninject.ts
@@ -1,9 +1,30 @@
+// Types
 import type { UtilInfo } from "../../typings";
 
-export default function uninject(utilInfo: UtilInfo) {
-  return new Promise<void>((resolve, reject) => {
-    console.log("UNINJECT:\n", utilInfo);
+// Modules
+import { rename, rm } from "fs/promises";
+import { join } from "path";
 
-    resolve();
-  });
+/**
+ * Moves the Guilded Asar Files back to their original location and deletes the _guilded directory.
+ *
+ * @param {UtilInfo} utilInfo Object containing information about file locations.
+ */
+async function handleAsarFiles(utilInfo: UtilInfo) {
+  try {
+    await rename(join(utilInfo.resourcesDir, "_guilded", "app.asar"), join(utilInfo.resourcesDir, "app.asar"));
+    await rename(
+      join(utilInfo.resourcesDir, "_guilded", "app.asar.unpacked"),
+      join(utilInfo.resourcesDir, "app.asar.unpacked")
+    );
+    await rm(join(utilInfo.resourcesDir, "_guilded"), { recursive: true, force: true });
+
+    return;
+  } catch (err) {
+    throw Error(err);
+  }
+}
+
+export default function uninject(utilInfo: UtilInfo) {
+  return Promise.all([handleAsarFiles(utilInfo), rm(join(utilInfo.resourcesDir, "app"), { recursive: true, force: true })]);
 }
