448e6cb97f89ade022070bf4a0161e5e9cdd0328
Nathaniel M <me@itznxthaniel.dev>
2023-01-31 23:46:54 -0800

    feat: UPDATE NOW WORKS! :tada:

--- src/injector/tasks/update.ts
+++ src/injector/tasks/update.ts
@@ -1,9 +1,31 @@
+// Types
 import type { UtilInfo } from "../../typings";
 
-export default function update(utilInfo: UtilInfo) {
-  return new Promise<void>((resolve, reject) => {
-    console.log("UPDATE:\n", utilInfo);
+// Modules
+import { access, constants, copyFile, mkdir } from "fs/promises";
+import { join } from "path";
 
-    resolve();
-  });
+/**
+ * Moves the `reguilded.asar` files that was built to the ReGuilded Directory.
+ *
+ * Creates the ReGuilded Directory if it doesn't exist yet.
+ * @param {UtilInfo} utilInfo Object containing information about file locations.
+ */
+async function accessAndCopyRG(utilInfo: UtilInfo) {
+  try {
+    await access(utilInfo.reguildedDir, constants.F_OK);
+
+    await copyFile(
+      join(__dirname, "reguilded.asar"),
+      join(utilInfo.reguildedDir, "reguilded.asar"),
+      constants.COPYFILE_FICLONE
+    );
+  } catch (e) {
+    await mkdir(utilInfo.reguildedDir);
+    return accessAndCopyRG(utilInfo);
+  }
+}
+
+export default async function update(utilInfo: UtilInfo) {
+  return accessAndCopyRG(utilInfo);
 }
